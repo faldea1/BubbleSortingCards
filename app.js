6734a4395ebc8a333a6cd91f5e57c2733b6bc64c
faldea1 <faldea1@uc.cl>
2022-12-13 01:01:40 +0000

    html ready

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ window.onload = () => {
     console.log("verificando...")
     console.log(DeckofCards());
 
+    
     //FUNCIONES PARA MOSTRAR UNA CARTA AL AZAR (N° y PALO):
 
     document.querySelector('.number').innerHTML = (RandomCardNumber());
 
-
-    let RandomCardSuits = RandomCardSuit() 
+    let RandomCardSuits = RandomCardSuit()
 
     document.querySelector('.t-suit').innerHTML = RandomCardSuits;
     document.querySelector('.b-suit').innerHTML = RandomCardSuits;
@@ -21,7 +21,6 @@ window.onload = () => {
 
 
 
-
 }
 
 
@@ -29,34 +28,24 @@ window.onload = () => {
 let RandomCardNumber = () => {
 
     let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+  
     let numberposition = Math.floor(Math.random() * numbers.length);
 
     return numbers[numberposition];
 
 };
 
+
+
 let RandomCardSuit = () => {
 
     let suits = ["♣", "♠", "♦", "♥"];
-
+   
     let suitsposition = Math.floor(Math.random() * suits.length);
 
     return suits[suitsposition];
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
