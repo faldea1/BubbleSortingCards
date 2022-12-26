2c4f2590ee54b55ed28f61796358dd069a26ed9f
faldea1 <faldea1@uc.cl>
2022-12-26 15:34:53 +0000

    cards & ref in js

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 
 window.onload = () => {
 
+    //Variable para Array de obj. suits:
+    const suits= [
+        {
+            name: 'Clubs',
+            color: 'black',
+            symbol: '♣'
+
+        },
+        {
+            name: 'Spades',
+            color: 'black',
+            symbol: '♠'
+
+        },
+        {
+            name: 'Hearts',
+            color: 'red',
+            symbol: '♥'
+
+        },
+        {
+            name: 'Diamonds',
+            color: 'red',
+            symbol: '♦'
+
+        },
+    ];
+
+    //Draw Button:
+    var prevnumb = document.getElementById('drawbutton');
+    var newnumb = prevnumb.cloneNode(true);
+    prevnumb.parentNode.replaceChild(newnumb, prevnumb);
 
 
-    
 
+    //Crear baraja de cartas
+    const generateCards = () => {
+        const cards = [];
+        suits.forEach(suit =>{
+            for (let i=2;)
+
+
+        });
+
+
+
+
+    }
 
 
 
